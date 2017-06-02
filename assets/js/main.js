1c03d4e9011fc69a0d8a854103cb0fe7b01bddec
kmdidar <kmdidar@gmail.com>
2017-06-02 19:54:10 +0600

    class35

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,20 +9,6 @@
         
         $('[data-toggle="tooltip"]').tooltip();
 
-        $(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.scrollToTop').fadeIn();
-		} else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
-	
-	//Click event to scroll to top
-	$('.scrollToTop').on('click', function() {
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
-
 
 
         
