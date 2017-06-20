3c83d4b9dfe5ca291a059de40df4cb777c0189dc
kmdidar <kmdidar@gmail.com>
2017-06-20 13:15:58 +0600

    class57

---
+++ class57/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $(".testimonial-athore-name").perfectScrollbar();
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
