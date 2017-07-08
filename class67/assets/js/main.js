80c5e701771c4feb41a9532b0ea1e7d6aceb8593
kmdidar <kmdidar@gmail.com>
2017-07-09 00:35:50 +0600

    class67

---
+++ class67/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        //Wow js
+		new WOW().init(); 
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
