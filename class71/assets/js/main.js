5e1e3dd7b7d7f10140acbe2e9e89285a97e03e66
kmdidar <kmdidar@gmail.com>
2017-07-18 22:38:02 +0600

    class71

---
+++ class71/assets/js/main.js
@@ -0,0 +1,38 @@
+(function($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+
+
+        $(window).on('scroll', function() {
+            if ($(this).scrollTop() > 50) {
+                $('.header-area').addClass('sticky');
+            } else {
+                $('.header-area').removeClass('sticky');
+            }
+        });
+
+        // var footerHifgt = $("footer.footer-area").height();
+        
+        $(".side-main-wraper").css('margin-bottom', 400 +'px');
+
+        $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="popover"]').popover();
+
+        
+
+
+
+
+
+
+    });
+
+
+    jQuery(window).load(function() {
+
+
+    });
+
+
+}(jQuery));
