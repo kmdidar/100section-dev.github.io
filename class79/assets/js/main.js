5a82f2b43df23b2a0f7358c61cce57c93b08b770
kmdidar <kmdidar@gmail.com>
2017-08-19 18:50:09 +0600

    cl79

---
+++ class79/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $('#mailchimp-form').ajaxChimp({
+        	url: 'http://kmdidarul.us16.list-manage.com/subscribe/post?u=cbb6fdf1a3718c97217c6fc70&amp;id=4d4c32188f'
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
